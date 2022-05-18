708a6c6498dbfbdcb7373f78a806b27d596053d1
DDeenis <ddeenis46@gmail.com>
2022-05-18 21:14:12 +0300

    use svelte

--- index.js
+++
@@ -1,9 +0,0 @@
-const scheduleTodayBtn = document.getElementById("schedule-today");
-
-scheduleTodayBtn.addEventListener("click", () => {
-  const newElem = document.createElement("button");
-  newElem.textContent = "Created using javascript";
-  newElem.classList.add("main-menu-actions__button");
-  scheduleTodayBtn.insertAdjacentElement("afterend", newElem);
-  scheduleTodayBtn.textContent = "Test";
-});
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import './styles.css'
+</script>
+
+<div class="main-container">
+  <h2 class="main-header">Действия</h2>
+  <section class="main-menu-actions">
+    <div class="main-menu-actions__header">Расписание</div>
+    <div class="main-menu-actions__actions">
+      <button class="main-menu-actions__button" id="schedule-today">
+        На сегодня
+      </button>
+      <button class="main-menu-actions__button" id="schedule-tomorrow">
+        На завтра
+      </button>
+      <button class="main-menu-actions__button" id="schedule-month">
+        На месяц
+      </button>
+    </div>
+  </section>
+</div>
