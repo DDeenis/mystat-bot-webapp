c0dd1de2797bea5ce00e21ea22e2be935514ef7f
DDeenis <ddeenis46@gmail.com>
2022-05-31 20:13:06 +0300

    new hook

--- src/components/Homeworks/HomeworkItem.svelte
+++ src/components/Homeworks/HomeworkItem.svelte
@@ -2,6 +2,17 @@
   export let item;
 </script>
 
-<div>
+<div class="hw-item-container">
   {item.name_spec}
 </div>
+
+<style>
+  .hw-item-container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  :global(.hw-item-container > *:not(:last-child)) {
+    margin-bottom: 0.35rem;
+  }
+</style>
