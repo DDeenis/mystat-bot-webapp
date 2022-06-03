be4a7fa47f35ddc0c35eae3f0a030bf11260a8b3
DDeenis <ddeenis46@gmail.com>
2022-06-03 20:50:48 +0300

    init HomeworkActive component

---
+++ src/components/Homeworks/HomeworkItem/HomeworkActive.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let item;
+</script>
+
+<div class="hw-item-container">
+  {item.name_spec}
+</div>
+
+<style>
+  .hw-item-container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  :global(.hw-item-container > *:not(:last-child)) {
+    margin-bottom: 0.35rem;
+  }
+</style>
