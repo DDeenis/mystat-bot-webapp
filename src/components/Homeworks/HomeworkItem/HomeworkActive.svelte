2f42ee38cc35c224f79782fced511b9ff0b2e621
DDeenis <ddeenis46@gmail.com>
2022-06-05 22:23:18 +0300

    start adding markup to active homeworks

--- src/components/Homeworks/HomeworkItem/HomeworkActive.svelte
+++ src/components/Homeworks/HomeworkItem/HomeworkActive.svelte
@@ -3,7 +3,13 @@
 </script>
 
 <div class="hw-item-container">
-  {item.name_spec}
+  ✏️ Предмет: {item.name_spec}
+  📖 Тема: {item.theme}
+  💡 Преподаватель: {item.fio_teach}
+  📅 Дата выдачи: {item.creation_time}
+  ❕ Сдать до: {item.completion_time}
+  ✒️ Комментарий: {item.comment}
+  <a href={item.file_path} rel="noopener noreferrer">📁 Путь к файлу</a>
 </div>
 
 <style>
