fc982fe8f823f3589271e131fec2dae691042a4b
DDeenis <ddeenis46@gmail.com>
2022-08-15 15:38:03 +0300

    third test (2)

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getUserByChatId } from "../../server/database/database";
+import userStore from "../../server/store/userStore";
 
 type ResponseData = {
   message?: string;
@@ -11,6 +12,13 @@ export default async function handler(
 ) {
   const chatId = req.body;
   const user = await getUserByChatId(chatId);
+
+  if (user) {
+    userStore.set(user.chatId, {
+      username: user.username,
+      password: user.password,
+    });
+  }
 
   res.status(200).json({ message: user?.username ?? "ERROR" });
 }
