f24d65006a4dcb1c861b8b7b12a0062fa124e48d
DDeenis <ddeenis46@gmail.com>
2023-07-07 18:43:40 +0100

    remove deleted import

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton/BackButton";
 import { Settings } from "../../components/Settings/Settings";
-import { getAllThemes } from "../../server/database/themes";
 
 export default async function SettingsPage() {
   return (
